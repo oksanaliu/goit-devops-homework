32aa1f90c44d982d57f67e8693b5ca59905c16d4
oksanaliu <lyuklyan4@icloud.com>
2026-01-26 00:48:24 +0100

    S3 encryption, backend, probes, limits

--- lesson-7/modules/s3-backend/s3.tf
+++ lesson-7/modules/s3-backend/s3.tf
@@ -1,8 +1,19 @@
 resource "aws_s3_bucket" "terraform_state" {
   bucket = var.bucket_name
-  tags = { Name = "Terraform State Storage" }
+  tags   = { Name = "Terraform State Storage" }
 }
+
 resource "aws_s3_bucket_versioning" "enabled" {
   bucket = aws_s3_bucket.terraform_state.id
   versioning_configuration { status = "Enabled" }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "state_encryption" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
 }
