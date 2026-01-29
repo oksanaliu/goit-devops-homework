213da81b34ae3739305fbe53bfd1673896be9277
oksanaliu <lyuklyan4@icloud.com>
2026-01-29 23:39:16 +0100

    Apply gitignore and cleanup untracked files

---
+++ lesson-8-9/modules/argo_cd/outputs.tf
@@ -0,0 +1,3 @@
+output "hostname" {
+  value = "argo-cd.${var.namespace}.svc.cluster.local"
+}
---
+++ lesson-8-9/modules/jenkins/variables.tf
@@ -0,0 +1,4 @@
+variable "namespace" {
+  description = "Namespace for Jenkins"
+  default     = "jenkins"
+}
