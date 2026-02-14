45b7a4d8f133d2d92bb29659da07b61bbeaf68cf
oksanaliu <lyuklyan4@icloud.com>
2026-02-14 14:39:51 +0100

    fixed

--- backend.tf
+++ backend.tf
@@ -1,11 +1,11 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "final-project-state-2026" 
-#     key            = "final-project/terraform.tfstate"
-#     region         = "eu-west-1"
+terraform {
+  backend "s3" {
+    bucket         = "final-project-state-2026" 
+    key            = "final-project/terraform.tfstate"
+    region         = "eu-west-1"
     
-#     dynamodb_table = "terraform-locks-final-project"
+    dynamodb_table = "terraform-locks-final-project"
     
-#     encrypt        = true
-#   }
-# }
+    encrypt        = true
+  }
+}
